51ac6b5f01f344c5b4edf73d1665251c5e0d3961
Paweł Marczewski <pawel@invisiblethingslab.com>
2022-04-04 14:42:32 +0200

    fixup! [LibOS] Add `type = "encrypted"` mounts
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- Pal/src/host/Linux-SGX/db_main.c
+++ Pal/src/host/Linux-SGX/db_main.c
@@ -623,11 +623,13 @@ static int print_warnings_on_insecure_configs(PAL_HANDLE parent_process) {
     bool allow_eventfd        = false;
     bool allow_all_files      = false;
     bool use_allowed_files    = g_allowed_files_warn;
-    bool enable_sysfs_topo    = false;
     bool protected_files_key  = false;
     bool encrypted_files_keys = false;
+    bool enable_sysfs_topo    = false;
 
     char* log_level_str = NULL;
+    char* protected_files_key_str = NULL;
+
     ret = toml_string_in(g_pal_public_state.manifest_root, "loader.log_level", &log_level_str);
     if (ret < 0)
         goto out;
@@ -662,20 +664,12 @@ static int print_warnings_on_insecure_configs(PAL_HANDLE parent_process) {
     if (get_file_check_policy() == FILE_CHECK_POLICY_ALLOW_ALL_BUT_LOG)
         allow_all_files = true;
 
-    ret = toml_bool_in(g_pal_public_state.manifest_root, "fs.experimental__enable_sysfs_topology",
-                       /*defaultval=*/false, &enable_sysfs_topo);
-    if (ret < 0)
-        goto out;
-
-    char* protected_files_key_str = NULL;
     ret = toml_string_in(g_pal_public_state.manifest_root, "sgx.insecure__protected_files_key",
                          &protected_files_key_str);
     if (ret < 0)
         goto out;
-    if (protected_files_key_str) {
-        free(protected_files_key_str);
+    if (protected_files_key_str)
         protected_files_key = true;
-    }
 
     toml_table_t* manifest_fs = toml_table_in(g_pal_public_state.manifest_root, "fs");
     if (manifest_fs) {
@@ -690,9 +684,14 @@ static int print_warnings_on_insecure_configs(PAL_HANDLE parent_process) {
         }
     }
 
+    ret = toml_bool_in(g_pal_public_state.manifest_root, "fs.experimental__enable_sysfs_topology",
+                       /*defaultval=*/false, &enable_sysfs_topo);
+    if (ret < 0)
+        goto out;
+
     if (!verbose_log_level && !sgx_debug && !use_cmdline_argv && !use_host_env && !disable_aslr &&
-            !allow_eventfd && !allow_all_files && !use_allowed_files && !enable_sysfs_topo &&
-            !protected_files_key && !encrypted_files_keys) {
+            !allow_eventfd && !allow_all_files && !use_allowed_files && !protected_files_key &&
+            !encrypted_files_keys && !enable_sysfs_topo) {
         /* there are no insecure configurations, skip printing */
         ret = 0;
         goto out;
@@ -734,17 +733,17 @@ static int print_warnings_on_insecure_configs(PAL_HANDLE parent_process) {
         log_always("  - sgx.allowed_files = [ ... ]                "
                    "(some files are passed through from untrusted host without verification)");
 
-    if (enable_sysfs_topo)
-        log_always("  - fs.experimental__enable_sysfs_topology = true "
-                   "(forwarding sysfs topology from untrusted host to the app)");
-
     if (protected_files_key)
-        log_always("  - sgx.insecure__protected_files_key = \"...\" "
+        log_always("  - sgx.insecure__protected_files_key = \"...\"  "
                    "(key hardcoded in manifest)");
 
     if (encrypted_files_keys)
-        log_always("  - fs.insecure__keys.* = \"...\"              "
+        log_always("  - fs.insecure__keys.* = \"...\"                "
                    "(keys hardcoded in manifest)");
+
+    if (enable_sysfs_topo)
+        log_always("  - fs.experimental__enable_sysfs_topology = true "
+                   "(forwarding sysfs topology from untrusted host to the app)");
 
     log_always("\nGramine will continue application execution, but this configuration must not be "
                "used in production!");
@@ -754,6 +753,7 @@ static int print_warnings_on_insecure_configs(PAL_HANDLE parent_process) {
     ret = 0;
 out:
     free(log_level_str);
+    free(protected_files_key_str);
     return ret;
 }
 
